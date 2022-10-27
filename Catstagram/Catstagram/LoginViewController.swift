03f2fc657b61a640f6042bc78217583ed7cb0038
KayAhn0126 <dkseotn123@gmail.com>
2022-10-28 01:19:05 +0900

    refactor: Login ViewController에서 SignUp ViewController 호출 구현

--- Catstagram/Catstagram/LoginViewController.swift
+++ Catstagram/Catstagram/LoginViewController.swift
@@ -10,26 +10,36 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var loginButton: UIButton!
-    
-    
     @IBOutlet weak var signUpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
     @IBAction func emailTextFieldEditingChanged(_ sender: UITextField) {
         if let currentText = sender.text {
             print(currentText)
         }
     }
     @IBAction func passwordTextFieldEditingChanged(_ sender: UITextField) {
+    
     }
+    
     @IBAction func loginButtonTapped(_ sender: UIButton) {
+    
     }
+    
     @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        // 화면 전환
+        // 1 스토리보드 생성
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // 2 뷰 컨트롤러 생성
+        let signUpViewController = storyboard.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
+        
+        // 3 화면전환 메서드 이용해 화면 전환
+        navigationController?.pushViewController(signUpViewController, animated: true)
     }
 }
---
+++ Catstagram/Catstagram/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  Catstagram
+//
+//  Created by Kay on 2022/10/28.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
