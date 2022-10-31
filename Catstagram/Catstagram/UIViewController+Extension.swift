fb619b2ed5e1fff5c057b5d119a1b459fd8b8fbb
KayAhn0126 <dkseotn123@gmail.com>
2022-10-31 23:30:10 +0900

    feat: 버튼내 일부 글자들의 속성을 바꾸기 위한 버튼 속성 변경 메서드 구현

---
+++ Catstagram/Catstagram/UIViewController+Extension.swift
@@ -0,0 +1,42 @@
+//
+//  UIViewController+Extension.swift
+//  Catstagram
+//
+//  Created by Kay on 2022/10/31.
+//
+
+import UIKit
+
+extension UIViewController {
+    func generateButtonAttribute(_ button: UIButton,
+                                 texts: String...,
+                                 fonts: UIFont...,
+                                 colors: UIColor...) -> NSMutableAttributedString {
+        // UIButton에 입력된 text를 가져온다.
+        guard let wholeText = button.titleLabel?.text else { fatalError("버튼에 텍스트가 없음.")}
+        // 폰트들
+        let customFonts: [UIFont] = fonts
+        
+        // 설정하고자 하는 String의 NSRange
+        let customTextsRanges = texts.indices.map { index in
+            (wholeText as NSString).range(of: texts[index])
+        }
+        
+        // 설정하고자 하는 색상들
+        let customColors = colors
+        
+        // attribute 객체를 생성한다.
+        let attributeddString = NSMutableAttributedString(string: wholeText)
+        
+        // 텍스트에 맞는 설정을 추가한다.
+        texts.indices.forEach { index in
+            attributeddString.addAttribute(.font,
+                                           value: customFonts[index],
+                                           range: customTextsRanges[index])
+            attributeddString.addAttribute(.font,
+                                           value: customColors[index],
+                                           range: customTextsRanges[index])
+        }
+        return attributeddString
+    }
+}
