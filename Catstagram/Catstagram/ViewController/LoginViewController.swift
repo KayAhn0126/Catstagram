9542aaee0fcae394cabe0078f96062852e977d9b
KayAhn0126 <dkseotn123@gmail.com>
2022-11-01 00:22:07 +0900

    refactor: LoginViewController내 signUp 버튼을 구간에 따라 색상 수정하는 코드 적용

--- Catstagram/Catstagram/ViewController/LoginViewController.swift
+++ Catstagram/Catstagram/ViewController/LoginViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
@@ -16,6 +16,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAttribute()
     }
     
     @IBAction func emailTextFieldEditingChanged(_ sender: UITextField) {
@@ -25,11 +26,11 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func passwordTextFieldEditingChanged(_ sender: UITextField) {
-    
+        
     }
     
     @IBAction func loginButtonTapped(_ sender: UIButton) {
-    
+        
     }
     
     @IBAction func signUpButtonTapped(_ sender: UIButton) {
@@ -42,5 +43,24 @@ class LoginViewController: UIViewController {
         
         // 3 화면전환 메서드 이용해 화면 전환
         navigationController?.pushViewController(signUpViewController, animated: true)
+    }
+    
+    private func setupAttribute() {
+        // setting attributes for signUpButton
+        let askQuestionText = "계정이 없으신가요?"
+        let signUpText = "가입하기"
+        
+        let askQuestionTextFont = UIFont.systemFont(ofSize: 13)
+        let signUpTextFont = UIFont.boldSystemFont(ofSize: 13)
+        
+        let askQuestionTextColor = UIColor.darkGray
+        let signUpTextColor = UIColor(named: "facebookColor")!
+        
+        let attributes = generateButtonAttribute(self.signUpButton,
+                                                 texts: askQuestionText, signUpText,
+                                                 fonts: askQuestionTextFont, signUpTextFont,
+                                                 colors: askQuestionTextColor, signUpTextColor)
+        
+        self.signUpButton.setAttributedTitle(attributes, for: .normal)
     }
 }
