9f12f361c5596f9716d97c1f126c62421dd75678
KayAhn0126 <dkseotn123@gmail.com>
2022-11-05 22:25:43 +0900

    feat: TableView내 첫번째 셀을 Feed로 활용하기 위한 Cell 구성 파일 및 xib 파일 추가

---
+++ Catstagram/Catstagram/ViewController/Home/FeedTableViewCell/FeedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedTableViewCell.swift
+//  Catstagram
+//
+//  Created by Kay on 2022/11/05.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
