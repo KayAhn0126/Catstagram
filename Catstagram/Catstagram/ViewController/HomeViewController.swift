0e1cc6f4a3b77194304c12bfdb86cf2ab654492c
KayAhn0126 <dkseotn123@gmail.com>
2022-11-05 17:20:52 +0900

    feat: HomeViewController 생성 및 연결

---
+++ Catstagram/Catstagram/ViewController/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Catstagram
+//
+//  Created by Kay on 2022/11/05.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
