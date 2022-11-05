a42ceb4dd6540ea38e3d51f5ea1ff14e64d21325
KayAhn0126 <dkseotn123@gmail.com>
2022-11-05 18:01:48 +0900

    refactor: 기본 테이블뷰 구성

--- Catstagram/Catstagram/ViewController/HomeViewController.swift
+++ Catstagram/Catstagram/ViewController/HomeViewController.swift
@@ -9,8 +9,28 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .blue
+        return cell
     }
 }
