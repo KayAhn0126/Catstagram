9a61e62973daaf1ba361154e8a7bf1147bb9a1b4
KayAhn0126 <dkseotn123@gmail.com>
2022-11-06 14:07:37 +0900

    feat: FeedTableViewCell 구현

--- Catstagram/Catstagram/ViewController/Home/HomeViewController.swift
+++ Catstagram/Catstagram/ViewController/Home/HomeViewController.swift
@@ -15,12 +15,17 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.separatorStyle = .none
+        let feedNib = UINib(nibName: "FeedTableViewCell", bundle: nil)
+        tableView.register(feedNib, forCellReuseIdentifier: "FeedTableViewCell")
     }
     
 }
 
 extension HomeViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 600
+    }
 }
 
 extension HomeViewController: UITableViewDataSource {
@@ -29,8 +34,10 @@ extension HomeViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.backgroundColor = .blue
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none
         return cell
     }
 }
