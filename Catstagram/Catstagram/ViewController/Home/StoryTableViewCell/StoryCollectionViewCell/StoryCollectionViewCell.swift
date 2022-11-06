6576caa515c39a677b476fdab9d30e7f9b671ab7
KayAhn0126 <dkseotn123@gmail.com>
2022-11-06 18:47:57 +0900

    feat: StoryCollectionViewCell 구현을 위한 파일 및 xib 파일 생성

---
+++ Catstagram/Catstagram/ViewController/Home/StoryTableViewCell/StoryCollectionViewCell/StoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  StoryCollectionViewCell.swift
+//  Catstagram
+//
+//  Created by Kay on 2022/11/06.
+//
+
+import UIKit
+
+class StoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
