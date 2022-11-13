7f29015ce6d3996f500762f6aea3aafd3022b11f
KayAhn0126 <dkseotn123@gmail.com>
2022-11-13 23:30:25 +0900

    chore: 주석 수정

--- Catstagram/Catstagram/ViewController/Home/FeedDataManager/FeedModel.swift
+++ Catstagram/Catstagram/ViewController/Home/FeedDataManager/FeedModel.swift
@@ -5,7 +5,7 @@
 //  Created by Kay on 2022/11/13.
 //
 
-// MARK: - 서버로 부터 response를 받아 
+// MARK: - 서버로 부터 response를 어떤 형태로 받을것인지.
 struct FeedModel: Decodable {
     var id: String
     var url: String
