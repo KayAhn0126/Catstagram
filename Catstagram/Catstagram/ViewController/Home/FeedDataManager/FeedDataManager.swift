fe14179f48ea38b7c1122ec3c9157f52b4ded380
KayAhn0126 <dkseotn123@gmail.com>
2022-11-13 23:29:19 +0900

    feat: 서버에 요청할 파라미터를 담아 실질적으로 네트워크 통신하는 manager 클래스 구현

---
+++ Catstagram/Catstagram/ViewController/Home/FeedDataManager/FeedDataManager.swift
@@ -0,0 +1,27 @@
+//
+//  FeedDataManager.swift
+//  Catstagram
+//
+//  Created by Kay on 2022/11/13.
+//
+
+import Alamofire
+
+class FeedDataManager {
+    var dataDelegate: DataTransferDelegate?
+    
+    init(_ dataDelegate: DataTransferDelegate? = nil) {
+        self.dataDelegate = dataDelegate
+    }
+    
+    func feedDataManager(_ parameters: FeedAPIInput) {
+        AF.request("https://api.thecatapi.com/v1/images/search", method: .get, parameters: parameters).validate().responseDecodable(of: [FeedModel].self) { response in
+            switch response.result {
+            case .success(let data):
+                self.dataDelegate!.sendData(data)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
