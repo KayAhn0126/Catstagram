37dd80dcf6dcb078e0cdc57d323ab5d07f4af776
KayAhn0126 <dkseotn123@gmail.com>
2022-12-08 00:23:05 +0900

    chore: delegate 자리에 nil이 들어올 수 있어 강제 언래핑을 옵셔널로 변경

--- Catstagram/Catstagram/ViewController/Home/FeedDataManager/FeedDataManager.swift
+++ Catstagram/Catstagram/ViewController/Home/FeedDataManager/FeedDataManager.swift
@@ -21,7 +21,7 @@ class FeedDataManager {
         AF.request("https://api.thecatapi.com/v1/images/search", method: .get, parameters: parameters).validate().responseDecodable(of: [FeedModel].self) { response in
             switch response.result {
             case .success(let data):
-                self.dataDelegate!.sendData(data)
+                self.dataDelegate?.sendData(data)
             case .failure(let error):
                 print(error.localizedDescription)
             }
